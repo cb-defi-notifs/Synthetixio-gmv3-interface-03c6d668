f5fef041be33171b729bc367d2dce6e7efe6ebe2
jmzwar <james@jmzwar.com>
2024-02-08 14:38:45 +1100

    move setup

--- jest.config.js
+++ jest.config.js
@@ -13,11 +13,7 @@ const customJestConfig = {
 	// if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
 	moduleDirectories: ['node_modules', '<rootDir>/'],
 	testEnvironment: 'jest-environment-jsdom',
-	setupFiles: ['jest-canvas-mock'],
-	global: {
-		TextEncoder: require('util').TextEncoder,
-		TextDecoder: require('util').TextDecoder,
-	},
+	setupFiles: ['jest-canvas-mock', './setup.jest.js'],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
