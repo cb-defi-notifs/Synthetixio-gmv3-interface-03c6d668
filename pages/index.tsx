4dc1c291b7346d7a6245852e1d9236b035a5d72b
MrX-SNX <max@cc.snxdao.io>
2024-08-16 09:09:44 +0100

    feat: added bg

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import { useColorMode } from '@chakra-ui/react';
-import LandingPage from 'components/LandingPage';
 import Main from 'components/Main';
 import type { NextPage } from 'next';
 import Head from 'next/head';
@@ -19,22 +18,31 @@ const Home: NextPage = () => {
 			<Head>
 				<title>Synthetix | Governance V3</title>
 			</Head>
-			<Main>
-				<div
-					style={{
-						color: 'black',
-						fontWeight: 700,
-						textAlign: 'center',
-						fontSize: '20px',
 
-						width: '100vw',
-						paddingTop: '50vh',
-					}}
-				>
-					🚧!!! Synthetix is in the process of migrating to V3 Governance on Synthetix Chain. This
-					webpage will soon be redirected to a new governance UI !!!🚧
-				</div>
-			</Main>
+			<img
+				src="/bg.svg"
+				style={{
+					position: 'absolute',
+					background: 'black',
+					width: '100vw',
+					height: '100vh',
+					objectFit: 'cover',
+					zIndex: -1,
+				}}
+			/>
+			<div
+				style={{
+					color: 'white',
+					fontWeight: 700,
+					textAlign: 'center',
+					fontSize: '20px',
+					width: '100vw',
+					paddingTop: '50vh',
+				}}
+			>
+				🚧!!! Synthetix is in the process of migrating to V3 Governance on Synthetix Chain. This
+				webpage will soon be redirected to a new governance UI !!!🚧
+			</div>
 		</>
 	);
 };
